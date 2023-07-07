a4c2d2c7e4deabb21cd0d1c0bc82a5ea1fe6a611
DAdawit <58243647+DAdawit@users.noreply.github.com>
2023-07-07 16:39:37 +0300

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import RentalFleets from "@/components/Home/RentalFleets";
 import RentalService from "@/components/Home/RentalService";
 import Image from "next/image";
 import Link from "next/link";
+import Footer from "@/common/Footer";
 
 export default function Home() {
   return (
@@ -24,6 +25,9 @@ export default function Home() {
         <ContactNumber />
         <CustomerReviews />
         <LatestNews />
+        <section className="bg-gray-900 mt-16 py-10">
+          <Footer />
+        </section>
       </main>
     </>
   );
